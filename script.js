ddd50a00008bd29560edd587cd98d70038499587
dayro florez <desing.dayroflorez@outlook.com>
2024-05-10 21:17:13 -0500

    agregar declarar variable

---
+++ script.js
@@ -0,0 +1,34 @@
+/* agregar comentario: alt + shift + a */
+/* comentario de bloque */
+// comentario de linea
+
+console.log("hola mundo")
+console.log("aqui podemos escribir cualquier cosa")
+console.log("adios")
+console.log("no hay autocompletado")
+console.log(12345)
+console.log(-1,2345)
+
+// Tipos de datos
+
+/* declarar una variable */
+let nombre = "dayro"
+console.log(nombre)
+monbre = "juan"
+console.log(nombre)
+
+/* declarar una constante */
+const pi = 3.1416
+console.log(pi)
+/* pi = 3 no se puede sobreescribir en una constante */
+
+/* javascrip antiguo */
+var apellido = "arias"
+
+const numero1 = 7
+const numero2 = 9
+const resultado = numero1 + numero2
+console.log("resultado:", resultado, "algo")
+
+
+
