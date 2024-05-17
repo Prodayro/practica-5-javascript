9d80b7381c5712df367de0978fa1ae487af6659d
dayro florez <desing.dayroflorez@outlook.com>
2024-05-17 15:19:22 -0500

    correccion de codigo funciones

--- script.js
+++ script.js
@@ -53,12 +53,12 @@ let esMorado = false
 
 //Array - Vectores - Listas
 let listaDeNumeros = [20, -55, 100]
-console.log(listaDeNumeros)
+//console.log(listaDeNumeros)
 
 // ver un elemento de la lista
-console.log(listaDeNumeros[0])
-console.log(listaDeNumeros[1])
-console.log(listaDeNumeros[2])
+// console.log(listaDeNumeros[0])
+// console.log(listaDeNumeros[1])
+// console.log(listaDeNumeros[2])
 
 
 let listaCiudades = [
@@ -68,47 +68,53 @@ let listaCiudades = [
     "cali"
 ]
 
-console.log(listaCiudades)
+// console.log(listaCiudades)
 
-let listaDeListas = 
-[listaDeNumeros, listaCiudades]
-console.log(listaDeListas)
+listaCiudades[2] = "boyaca"
+// console.log(listaCiudades)
 
-// ----------------acceder al  dato "medellin"------------
-console.log(listaDeListas[1][1])
+let listaDeListas = [listaDeNumeros, listaCiudades]
+// console.log(listaDelistas)
 
-// ----------------JSON (javaScript Objet Notation) - Objetos
-// ----------------clave valor
-let usuario = {nombre: "dayro",
- edad: 35,
- ciudad: "bogotá",
- ubicación: {
-    latitud:1.53565656,
-    longitud:-2.3486545
+// acceder al  dato "medellin"
+//console.log(listaDeListas[1][1])
+
+// JSON (javaScript Objet Notation) - Objetos
+// clave valor
+
+let usuario = {
+    nombre: "dayro",
+    edad: 35,
+    ciudad: "bogotá",
+    ubicación: {
+        latitud:1.53565656,
+        longitud:-2.3486545
  },
  amigos: ["andres", "pepito"]
 }
 
-console.log(usuario)
-console.log(usuario.nombre)
-console.log(usuario.ubicación.latitud)
+// console.log(usuario)
+// console.log(usuario.nombre)
+// console.log(usuario.ubicación.latitud)
 
 usuario.edad = 25
-console.log(usuario)
+// console.log(usuario)
 
-//-------------tipos de datos vacios --------
-//--null - nulo-
+// ----------tipos de datos vacios----------
 
+//null - nulo
 let espacioVacio = null
 
-//------undefined - indefinido------
+// undefined - indefinido
 let noDefinido = undefined
-console.log(usuario.apellido)
+// console.log(usuario.apellido)
+let algo
+// console.log(algo)
 
-//------ NaN -not a Number - no es un numero------
+// NaN - not a Number - no es un numero
 const noEsNumero = NaN
 const multiplicacion = 10 * "hola"
-console.log(multiplicacion)
+//console.log(multiplicacion)
 
 // -------estructuras de control --------
 
@@ -124,7 +130,7 @@ const listaAnimales = [
     "zorro"
 ]
 
-console.log("tamaño del array", listaAnimales.length)
+//console.log("tamaño del array", listaAnimales.length)
 
 //parametro 1: contador
 //parametro 2: condición para que se detenga
@@ -138,11 +144,90 @@ for(let contador = 0; i < listaAnimales.length; i++) {
 
 //condiciones
 // - igualdad: ===
+// - diferente de: !==
 // - menor que: <
 // - mayor que: >
 // - menor a igual que: <=
 // - mayor o igual que: >=
 
+let numero = 1
+
+if (numero === 10) {
+    console.log("numero es 10")
+} else if (numero === 11){
+    console.log("numero es 11")
+} else if (numero === 12) {
+    console.log("mumero es 12")
+} else {
+    console.log("no es ni 10 ni 11 ni 12")
+}
+
+//operadores logicos
+// and - y - &&
+// or - o - ||
+let texto = "c"
+
+if (texto === "a" || texto === "b" || texto === "c") {
+    console.log("la condicion es verdadera")
+}   else {
+        console.log("el texto no es ni 'a' ni 'b'")
+}
+
+// and - y - &&
+let numero3 = 20
+    
+if (numero3 > 0 && numero3 <10) {
+    console.log("nuestro numero esta entre el 0 y el 10")
+} else {
+    console.log("nuestro numero no esta entre el 0 y el 10")
+}
+
+
+// ----------bucles - while - mientras----------
+
+let contador = 0
+while (contador < 5 ) { //ciclo 3 - contador = 2
+        console.log("ciclo;", contador)
+        console.log("animal", listaAnimales[contador])
+        
+        // contador = contador + 1
+        // contador++ 
+}
+
+    // ----------funciones----------
+    // estructuras reciben parametros, procesarlos
+    // y devuelven parametros
+
+    // caracteristicas
+    // 1. agrupar codigo relacionado a un cierto fin
+    // 2. la funcion es reutilizable
+
+    // declaracion de la funcion javascript Antiguo
+    // function sumar(valor1, valor2) {
+       // let resultado = valor1 + valor2
+        // return resultado
+    // }
+
+    // declaracion de la funcion con ECMAScript
+    // funcion flecha
+    const sumar = (valor1, valor2) =>{
+        let resiltado = valor1 + valor2
+        return resultado
+    }
+    
+    // ejecicion de la funcion \\ llamado de la funcion
+
+     let resultadoSuma = sumar(5, 10)
+    console.log("resultad de la suma1", resultadoSuma)
+
+    console.log("resultad dela suma2", sumar(50, 15))
+    console.log("resultad dela suma2", sumar(100, 8))
+    console.log("resultad dela suma2", sumar(20, 1))
+    console.log("resultad dela suma2", sumar(5, 2)) 
+    
+ 
+
+ 
 
 
 
